009796410b48e6a5119eda0060264ee6b00468d6
Shubham Jangid <shubham.jangid@beehyv.com>
2024-07-11 14:59:31 +0530

    added content to the site

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
+
 
 def say_hello(request):
     return HttpResponse('HelloWorld')
@@ -9,4 +12,5 @@ def output_gif(request):
 def image_show(request):
     return HttpResponse('No idea how to get an image')
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
