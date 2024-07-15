80081af3bcf8ccff28935291888c6ef21e27dfa3
Shubham Jangid <shubham.jangid@beehyv.com>
2024-07-15 17:54:37 +0530

    assignment 3

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,12 @@ urlpatterns = [
     path('hello/',views.say_hello),
     path('giffo/',views.output_gif),
     path('laugho/',views.image_show),
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('create_blog/',views.create_view),
+    path('list_blog/',views.list_view),
+    path('list_blog/<id>',views.detail_view),
+    path('<id>', views.update_view),
+    path('<id>/delete/', views.delete_view),
+    
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import (get_object_or_404,
+                              render,
+                              HttpResponseRedirect)
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Post
-
+from .forms import post_form
 
 def say_hello(request):
     return HttpResponse('HelloWorld')
@@ -12,5 +14,37 @@ def output_gif(request):
 def image_show(request):
     return HttpResponse('No idea how to get an image')
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+def create_view(request):
+    context = {}
+    form = post_form(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context['form'] =  form
+    return render(request, "blog/create_view.html", context) 
+def list_view(request):
+    context = {}
+    context["dataset"] = Post.objects.all()
+    return render(request , "blog/list_view.html",context)
+def detail_view(request,id):
+    context={}
+    context["data"] = Post.objects.get(id=id)
+    return render(request,"blog/detail_view.html", context)
+def update_view(request, id):
+    context  = {}
+    obj = get_object_or_404(Post, id = id)
+    form  = post_form(request.POST or None, instance = obj)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect("/"+id)
+    context["form"] = form
+    return render(request , "blog/update_view.html" , context)
+def delete_view(request, id):
+    context ={}
+    obj = get_object_or_404(Post, id = id)
+    if request.method =="POST":
+        obj.delete()
+        return HttpResponseRedirect("/")
+    return render(request, "blog/delete_view.html", context)
+
